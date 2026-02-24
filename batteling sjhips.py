3269be36f4f0e05b3989c360d4f40aaff80c664e
Levon-levon <23288@wc.school.nz>
2026-02-25 09:40:44 +1300

    my files?

--- batteling sjhips.py
+++ batteling sjhips.py
@@ -9,7 +9,7 @@ board = [["","","","",""],
 
 
 
-
+player_1_placement = True
 
 #---------------start code---------------
 
@@ -17,10 +17,26 @@ board = [["","","","",""],
 while True:
     player_1_placement_row = int(input("which row do you want to place your ship"))
     player_1_placement_column = int(input("which column do you place your ship in "))
-    board[player_1_placement_row][player_1_placement_column] = "S1"
-    player_1_placement_column += 1
-    board[player_1_placement_row][player_1_placement_column] = "S1"
-    
+
+    while player_1_placement == True:
+        if player_1_placement_row <=4:
+            try:
+                board[player_1_placement_row][player_1_placement_column] = "S1"
+                next_placement = True
+            except:
+                print("placement outside of board try again")
+                break
+        if next_placement == True:
+            try:
+                player_1_placement_column += 1
+                board[player_1_placement_row][player_1_placement_column] = "S1"
+                break
+            except:
+                print("placement outside of board try again")
+                break
+
+
+
     for i in range(0,5):
         print(board[i])
 
