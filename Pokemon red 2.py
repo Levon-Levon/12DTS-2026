8f80dcb2de0e998f6fef19b0cb3f2030fbcb65ec
Levon-levon <23288@wc.school.nz>
2026-02-27 14:04:28 +1300

    pushhhhh

--- Pokemon red 2.py
+++ Pokemon red 2.py
@@ -1,5 +1,43 @@
 
+import random
+
+
+matrix = [{"thing":"other"},{"thing2","other2"}]
+
+wild_pokemon = [
+    {"Name":"Charizard","Type":"Fire","Level":random.randint(1,5),"Health":random.randint(50,100)},
+    {"Name":"Vaporeon","Type":"Water","Level":random.randint(1,7),"Health":random.randint(10,30)},
+    {"Name":"diglet","Type":"Earth","Level":random.randint(1,2),"Health":random.randint(7,24)},
+    {"Name":"Raichu","Type":"Electric","Level":random.randint(1,3),"Health":random.randint(12,28)},
+    {"Name":"Mr Mime","Type":"psychic","Level":random.randint(1,9),"Health":random.randint(15,40)},
+    {"Name":"Jigglypuff","Type":"Fiary","Level":random.randint(1,4),"Health":random.randint(35,68)},
+    {"Name":"Lopunny","Type":"Normal","Level":random.randint(1,10),"Health":random.randint(4,17)},
+
+
+]
+
+
+def battle():
+    x = random.randint(0,len(wild_pokemon)-1)
+    pokemon = wild_pokemon[x]
+    print("you encountered a wild",pokemon["Name"])
+    print("its a",pokemon["Type"], "type pokemon")
+    print("its level is",pokemon["Level"])
+    print("it has",pokemon["Health"],"health")
 
 
 
-matrix = [{"shivam":"chodi"},{"thing2","other2"}]
+
+battle()
+
+
+
+
+
+
+
+
+
+
+
+
